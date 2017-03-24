fb950f496ae8a62121d19c552e9a91682507d87e
Julio Faerman <jfaerman@gmail.com>
2017-03-24 14:35:10 +0100

    Initial API, second twitch broadcast

--- breakless-react/src/App.js
+++ breakless-react/src/App.js
@@ -3,7 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props, context){
+    super(props, context);
+    this.state = {
+        items: []
+    }
+  }
+
   render() {
+    const {items} = this.state;
+
     return (
       <div className="App">
         <div className="App-header">
@@ -13,9 +22,32 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        { items.map(item => <p>{item.id}</p>) }
       </div>
     );
   }
+
+  componentDidMount(){
+    var that = this;
+    console.log("Component mounted!");
+    var apigClient = window.apigClientFactory.newClient();
+    var params = {};
+    var body = {};
+    var additionalParams = {};
+    console.log("before call");
+
+    apigClient.featureditemsGet(params, body, additionalParams)
+        .then(function(result) {
+            console.log("success");
+            console.log(result.data);
+            that.setState(result.data);
+        }).catch(function(error) {
+          console.log("error");
+          console.error(error);
+        });
+  }
+
 }
 
 export default App;
